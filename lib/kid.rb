fe97d40d05bd97c6e9d7b4e85a0adedae3251883
Tanya Hodges <tanyac52003@gmail.com>
2020-06-11 18:31:17 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
+  class Kid
+    extend MetaDancing 
+  end 
   include Dance
  
   attr_accessor :name
