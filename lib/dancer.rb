f5947f17354b271ab137fa777f975e63840c4771
Tanya Hodges <tanyac52003@gmail.com>
2020-06-11 18:39:11 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,19 +3,13 @@ require_relative './dance_module.rb'
 
 class Dancer 
   include Dance
+  extend MetaDancing
  
    attr_accessor :name
  
   def initialize(name)
     @name = name
   end
-end
-
-  class Dancer 
-    extend MetaDancing
-  end 
-  
- 
- 
+end 
 
 end 
