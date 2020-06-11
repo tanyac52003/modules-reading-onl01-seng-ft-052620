f550a5b1e3bf5e40875c1b6754c7060d391a097f
Tanya Hodges <tanyac52003@gmail.com>
2020-06-11 18:45:21 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,6 +5,11 @@ require_relative './fancy_dance.rb'
 class Dancer 
   include Dance
   extend MetaDancing
+  
+  class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
  
    attr_accessor :name
  
